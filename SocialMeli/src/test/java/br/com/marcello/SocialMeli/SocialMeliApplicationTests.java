1454afbac1a5ba02909bc89487335213c5905c00
Marcello Farabolini <marcello.mfarabolini@mercadolivre.com>
2021-06-01 11:04:45 -0300

    First commit

---
+++ SocialMeli/src/test/java/br/com/marcello/SocialMeli/SocialMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcello.SocialMeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
